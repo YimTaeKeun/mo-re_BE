2b5368f7e3f84023b127e85566b6b9729fafacd0
YimTaeKeun <ytk030305@gmail.com>
2025-01-03 17:15:18 +0900

    FEAT: User 모델 추가 & 기본 auth 모델로 설정

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'social_auth'
+    'social_auth',
+    'usr',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'usr.User' # usr의 User를 기본 auth 모델로 적용
