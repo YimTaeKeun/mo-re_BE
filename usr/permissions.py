720542abeaf8e8800ad96ff0b0b9c8e5fd9eb851
YimTaeKeun <ytk030305@gmail.com>
2025-01-05 16:27:08 +0900

    Add Staff Permission

--- usr/permissions.py
+++ usr/permissions.py
@@ -10,3 +10,11 @@ class IsOwnerOrReadOnly(BasePermission):
             # TODO 해당 퍼미션이 정확하게 동작하는지는 실전 배포에 가야 테스트 가능
             # 현재로선 익명 유저와 인증된 유저 사이의 차이점 구분하는 데에는 성공, 인증된 유저가 게시물 등록하다는 것까지 성공
         )
+
+# 관리자만 접근하도록 허가하는 퍼미션 클래스 입니다.
+class IsStaff(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user and request.user.is_authenticated and # 인증된 사용자인가
+            request.user.is_staff # 그 인증된 사용자가 관리자인가
+        )
