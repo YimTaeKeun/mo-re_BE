a13846719c86eefa0b750c2768c97ddf274d5183
YimTaeKeun <ytk030305@gmail.com>
2025-01-10 17:52:05 +0900

    FEAT: Create Api Who is Me

--- usr/tests.py
+++ usr/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+
+ACCESS_TOKEN = 'N_tlzdsd16mgXflb9o0O4N2VwwN4y-ObAAAAAQoqJREAAAGUT2HQHeQ1KlcE_6bt'
+
+class UserTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # 테스트 사용자 정보를 등록해줍니다.
+        User.objects.create(
+            sub=3857463839,
+            username='임태근'
+        )
+
+    def test_specifying_user(self): # 액세스 토큰 유저 정보 반환 api를 테스트합니다.
+        headers = {'Authorization': 'Bearer ' + ACCESS_TOKEN}
+        print(headers)
+        response = self.client.get('/user/me/', headers=headers)
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
--- usr/views.py
+++ usr/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from .models import User
+from rest_framework.response import Response
 
 # Create your views here.
+
+# 로그인한 사용자가 누구인지 반환합니다.
+class SpecifyingUserViewSet(viewsets.ViewSet):
+    def retrieve(self, request, *args, **kwargs):
+        if request.user.is_authenticated: # 로그인된 사용자라면
+            return Response({
+                "sub": request.user.sub, # 사용자 고유 id를 반환합니다.
+                "username": request.user.username, # 사용자 닉네임 정보를 반환합니다.
+            }, status=status.HTTP_200_OK)
+        return Response({"Error": "로그인 해주세요."}, status=status.HTTP_403_FORBIDDEN)
